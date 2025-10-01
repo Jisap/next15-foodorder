5bc8ea7b53b0dbed157257c0ce7fba894bcaef8c
Jorge Matos <jisapjisap@gmail.com>
2025-10-01 17:47:48 +0200

    Update package dependencies and enhance FruitYogurtSmoothies component with animations; integrate framer-motion for improved user experience and update styling for better presentation.

--- src/Components/FruitYogurtSmoothies.jsx
+++ src/Components/FruitYogurtSmoothies.jsx
@@ -1,9 +1,10 @@
 "use client"
 
 import juiceData from "@/constants/Data.json"
-import { Eye, Heart, ShoppingCart } from "lucide-react";
+import { Eye, Heart, ShoppingCart } from "lucide-react"
 import Image from "next/image";
 import { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 
 const FruitYogurtSmoothies = () => {
 
@@ -17,10 +18,26 @@ const FruitYogurtSmoothies = () => {
     "Mocktails"
   ]
 
+  const containerVariants = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.1
+      }
+    }
+  };
+  
+  const itemVariants = {
+    hidden: { y: 20, opacity: 0 },
+    visible: { y: 0, opacity: 1 }
+  };
+  
+
   return (
     <div className='section-fruit relative juice-wrap pb-[100px] px-[8%] lg:px-[12%] overflow-hidden'>
       <div className="title flex flex-col justify-center items-center text-center pt-[5%]">
-        <h1 className="font-bold font-fraunces text-6xl text-white mt-12">
+        <h1 className="font-bold font-fraunces text-6xl text-white mt-12 [text-shadow:5px_-2px_0_var(--fourth-color)]">
           Fruit & Yogurt Smoothies
         </h1>
 
@@ -42,43 +59,52 @@ const FruitYogurtSmoothies = () => {
           ))}
         </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 my-10">
-          {juiceData.slice(activeIndex * 6, activeIndex * 6 + 6).map((item, index) => (
-            <div key={item.id || index} className="juice-card group relative flex flex-col justify-center items-center text-center mb-16 cursor-pointer"> 
-              <div className="juice-img relative">
-                <Image 
-                  src={item.image} 
-                  alt={item.title} 
-                  width={300} 
-                  height={210} 
-                  className="rounded-full"
-                />
+        <AnimatePresence mode="wait">
+          <motion.div
+            key={activeIndex} // Clave para que AnimatePresence detecte el cambio
+            variants={containerVariants}
+            initial="hidden"
+            animate="visible"
+            exit="hidden"
+            className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 my-10"
+          >
+            {juiceData.slice(activeIndex * 6, activeIndex * 6 + 6).map((item, index) => (
+              <motion.div key={item.id || index} variants={itemVariants} className="juice-card group relative flex flex-col justify-center items-center text-center mb-16 cursor-pointer"> 
+                <div className="juice-img relative">
+                  <Image 
+                    src={item.image} 
+                    alt={item.title} 
+                    width={300} 
+                    height={210} 
+                    className="rounded-full"
+                  />
 
-                <div className="absolute inset-0 flex items-center justify-center gap-4 opacity-0 group-hover:opacity-100 transition-opacity">
-                  <div className="bg-yellow-100 p-3 rounded-full cursor-pointer hover:bg-yellow-300 transition">
-                    <Heart className="w-6 h-6 text-gray-800 hover:text-black"/>
-                  </div>
-                  <div className="bg-yellow-100 p-3 rounded-full cursor-pointer hover:bg-yellow-300 transition">
-                    <ShoppingCart className="w-6 h-6 text-gray-800 hover:text-black"/>
-                  </div>
-                  <div className="bg-yellow-100 p-3 rounded-full cursor-pointer hover:bg-yellow-300 transition">
-                    <Eye className="w-6 h-6 text-gray-800 hover:text-black"/>
+                  <div className="absolute inset-0 flex items-center justify-center gap-4 opacity-0 group-hover:opacity-100 transition-opacity">
+                    <div className="bg-yellow-100 p-3 rounded-full cursor-pointer hover:bg-yellow-300 transition">
+                      <Heart className="w-6 h-6 text-gray-800 hover:text-black"/>
+                    </div>
+                    <div className="bg-yellow-100 p-3 rounded-full cursor-pointer hover:bg-yellow-300 transition">
+                      <ShoppingCart className="w-6 h-6 text-gray-800 hover:text-black"/>
+                    </div>
+                    <div className="bg-yellow-100 p-3 rounded-full cursor-pointer hover:bg-yellow-300 transition">
+                      <Eye className="w-6 h-6 text-gray-800 hover:text-black"/>
+                    </div>
                   </div>
                 </div>
 
                 <div className="juice-info mt-8">
-                  <h1 className="font-semibold font-fraunces text-2xl text-white">
-                    {item.title}
-                  </h1>
+                    <h1 className="font-semibold font-fraunces text-2xl text-white">
+                      {item.title}
+                    </h1>
 
-                  <p className="text-white font-fraunces font-semibold text-xl">
-                    {item.price}
-                  </p>
+                    <p className="text-white font-fraunces font-semibold text-xl">
+                      {item.price}
+                    </p>
                 </div>
-              </div>
-            </div>
-          ))}
-        </div>
+              </motion.div>
+            ))}
+          </motion.div>
+        </AnimatePresence>
 
         <div className="mt-5 flex justify-center items-center">
           <button className="bg-[#ffc935] px-8 py-3 pb-4 text-xl rounded-full hover:bg-white transition-all duration-300">
