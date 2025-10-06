7840e19ca5e7523507fea51e2709a8e3d0684a14
Jorge Matos <jisapjisap@gmail.com>
2025-10-06 13:54:29 +0200

    Refactor TestimonialsComponent to utilize testimonialsData array for dynamic rendering of testimonials; streamline code by removing hardcoded testimonial entries and enhancing maintainability.

--- src/Components/TestimonialsComponent.jsx
+++ src/Components/TestimonialsComponent.jsx
@@ -8,6 +8,33 @@ import 'swiper/css/effect-fade';
 import 'swiper/css/pagination';
 
 
+const testimonialsData = [
+  {
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+    author: "Laara",
+    role: "Teacher",
+    image: test1,
+  },
+  {
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+    author: "Karen",
+    role: "Economist",
+    image: test2,
+  },
+  {
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+    author: "Greg",
+    role: "Student",
+    image: test3,
+  },
+  {
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+    author: "Jasmine",
+    role: "Developer",
+    image: test4,
+  },
+];
+
 const TestimonialsComponent = () => {
   return (
     <div className="section-testimonials pb-[100px] px-[8%] lg:px-[12%]">
@@ -30,173 +57,46 @@ const TestimonialsComponent = () => {
         }}
         className="mt-10"
       >
-        <SwiperSlide>
-          <div className="test-card flex flex-col items-center lg:flex-row gap-12">
-            <div className="text-content lg:w-8/12 w-full">
-              <p className="text-white text-lg test-pere">
-                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
-                  ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in 
-                  voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-              </p>
-
-              <div className="test-box flex justify-between">
-                <div className="test-info flex items-center gap-3 mt-4 text-white">
-                  <h2 className="text-3xl font-bold">
-                    Laara
-                  </h2>
-
-                  <p className="m-0 pt-2">- Teacher</p>
+        {testimonialsData.map((testimonial, index) => (
+          <SwiperSlide key={index}>
+            <div className="test-card flex flex-col items-center lg:flex-row gap-12">
+              <div className="text-content lg:w-8/12 w-full">
+                <p className="text-white text-lg test-pere">
+                  "{testimonial.text}
+                </p>
+  
+                <div className="test-box flex justify-between">
+                  <div className="test-info flex items-center gap-3 mt-4 text-white">
+                    <h2 className="text-3xl font-bold">
+                      {testimonial.author}
+                    </h2>
+  
+                    <p className="m-0 pt-2">- {testimonial.role}</p>
+                  </div>
+                  <Image 
+                    src={quat}
+                    alt="quat"
+                    className="mr-10"
+                  />
                 </div>
-
+              </div>
+              <div className="test-image lg:w-4/12 w-full">
                 <Image 
-                  src={quat}
-                  alt="quat"
-                  className="mr-10"
+                  src={testimonial.image} 
+                  alt={testimonial.author} 
+                />
+                <Image 
+                  src={garpsImg} 
+                  alt="garpsImg" 
+                  className="absolute bottom-0 left-0"
+                  style={{
+                    width: "200px"
+                  }}
                 />
               </div>
             </div>
-            <div className="test-image lg:w-4/12 w-full">
-              <Image 
-                src={test1} alt="test1" 
-              />
-              <Image 
-                src={garpsImg} 
-                alt="garpsImg" 
-                className="absolute bottom-0 left-50"
-                style={{
-                  width: "200px"
-                }}
-              />
-            </div>
-          </div>
-        </SwiperSlide>
-
-        <SwiperSlide>
-          <div className="test-card flex flex-col items-center lg:flex-row gap-12">
-            <div className="text-content lg:w-8/12 w-full">
-              <p className="text-white text-lg test-pere">
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-                ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-                voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-              </p>
-
-              <div className="test-box flex justify-between">
-                <div className="test-info flex items-center gap-3 mt-4 text-white">
-                  <h2 className="text-3xl font-bold">
-                    Karen
-                  </h2>
-
-                  <p className="m-0 pt-2">- Economist</p>
-                </div>
-
-                <Image
-                  src={quat}
-                  alt="quat"
-                  className="mr-10"
-                />
-              </div>
-            </div>
-            <div className="test-image lg:w-4/12 w-full">
-              <Image
-                src={test2} alt="test1" className="rounded-full"
-              />
-              <Image
-                src={garpsImg}
-                alt="garpsImg"
-                className="absolute bottom-0 left-50"
-                style={{
-                  width: "200px"
-                }}
-              />
-            </div>
-          </div>
-        </SwiperSlide>
-
-        <SwiperSlide>
-          <div className="test-card flex flex-col items-center lg:flex-row gap-12">
-            <div className="text-content lg:w-8/12 w-full">
-              <p className="text-white text-lg test-pere">
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-                ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-                voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-              </p>
-
-              <div className="test-box flex justify-between">
-                <div className="test-info flex items-center gap-3 mt-4 text-white">
-                  <h2 className="text-3xl font-bold">
-                    Greg
-                  </h2>
-
-                  <p className="m-0 pt-2">- Student</p>
-                </div>
-
-                <Image
-                  src={quat}
-                  alt="quat"
-                  className="mr-10"
-                />
-              </div>
-            </div>
-            <div className="test-image lg:w-4/12 w-full">
-              <Image
-                src={test3} alt="test1" className="rounded-full"
-              />
-              <Image
-                src={garpsImg}
-                alt="garpsImg"
-                className="absolute bottom-0 left-50"
-                style={{
-                  width: "200px"
-                }}
-              />
-            </div>
-          </div>
-        </SwiperSlide>
-
-        <SwiperSlide>
-          <div className="test-card flex flex-col items-center lg:flex-row gap-12">
-            <div className="text-content lg:w-8/12 w-full">
-              <p className="text-white text-lg test-pere">
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-                ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-                voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-              </p>
-
-              <div className="test-box flex justify-between">
-                <div className="test-info flex items-center gap-3 mt-4 text-white">
-                  <h2 className="text-3xl font-bold">
-                    Jasmine
-                  </h2>
-
-                  <p className="m-0 pt-2">- Developer</p>
-                </div>
-
-                <Image
-                  src={quat}
-                  alt="quat"
-                  className="mr-10"
-                />
-              </div>
-            </div>
-            <div className="test-image lg:w-4/12 w-full">
-              <Image
-                src={test4} alt="test1" className="rounded-full"
-              />
-              <Image
-                src={garpsImg}
-                alt="garpsImg"
-                className="absolute bottom-0 left-50"
-                style={{
-                  width: "200px"
-                }}
-              />
-            </div>
-          </div>
-        </SwiperSlide>
+          </SwiperSlide>
+        ))}
       </Swiper>
     </div>
   )
